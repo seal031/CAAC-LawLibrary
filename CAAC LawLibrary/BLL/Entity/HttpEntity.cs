06b8517c4449b91f9638aa0fe974ff5c49411630
QIKEXIN <87749356@qq.COM>
2018-08-09 00:22:26 +0800

    20180809

---
+++ CAAC LawLibrary/BLL/Entity/HttpEntity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAAC_LawLibrary.BLL.Entity
+{
+    /// <summary>
+    /// 提交征询post参数对象
+    /// </summary>
+    public class ConsultRequest
+    {
+        public string bookId { get; set; }
+        public string userId { get; set; }
+        public string nodeId { get; set; }
+        public string text { get; set; }
+        public string mark { get; set; }
+    }
+
+    /// <summary>
+    /// 获取意见get参数对象
+    /// </summary>
+    public class OpinionRequest
+    {
+        public string bookId { get; set; }
+    }
+    /// <summary>
+    /// 提交用户意见post参数对象
+    /// </summary>
+    public class OpinionCommitRequest
+    {
+        public string bookId { get; set; }
+        public string nodeId { get; set; }
+        public string userId { get; set; }
+        public string msg { get; set; }
+    }
+
+    /// <summary>
+    /// 获取法规列表get参数对象
+    /// </summary>
+    public class AllBooksRequest
+    {
+        public int pageSize { get; set; }
+        public int pageNum { get; set; }
+        public bool history { get; set; }
+    }
+
+
+    /// <summary>
+    /// 通用返回对象
+    /// </summary>
+    public class CommonResponse
+    {
+        public string status { get; set; }
+        public string errmsg { get; set; }
+        public string data { get; set; }
+    }
+}
--- CAAC LawLibrary/Utity/Global.cs
+++ CAAC LawLibrary/Utity/Global.cs
@@ -20,4 +20,23 @@ namespace CAAC_LawLibrary.Utity
             SortSource.Add(new DictionaryEntry(2, "首字母拼音排序"));
         }
     }
+
+    public class UTC
+    {
+        public static int ConvertDateTimeInt(System.DateTime time)
+        {
+            double intResult = 0;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            intResult = (time - startTime).TotalSeconds;
+            return (int)intResult;
+        }
+
+        public static DateTime ConvertIntDatetime(double utc)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            startTime = startTime.AddSeconds(utc);
+            startTime = startTime.AddHours(8);//转化为北京时间(北京时间=UTC时间+8小时 )    
+            return startTime;
+        }
+    }
 }
